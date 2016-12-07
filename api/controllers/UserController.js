55b70db72f74db810830e9a3dfd1012fde52745b
Rachael Shaw <r@rachael.wtf>
2016-12-07 12:30:35 -0600

    Generate User model/controller to match structure of anatomy documentation

---
+++ api/controllers/UserController.js
@@ -0,0 +1,11 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
